1fcfcc7608dbab8a9345dadc70fab7feee7a41aa
hillarioh <okerioh@gmail.com>
2020-08-30 09:03:01 +0300

    basic 1

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import 'phaser'; 
+
+const gameState = {};
+
+function preload(){
+  this.load.image('codey','https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/codey.png');
+}
+
+function create(){
+  gameState.codey = this.add.sprite(100,100,'codey');
+  gameState.cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update(){
+  if(gameState.cursors.down.isDown){
+    gameState.codey.y +=1;
+  }
+}
+
+let config = {
+  width: 400,
+  height: 600,
+  backgroundColor: "0xdda0dd",
+  scene: {
+    preload,
+    create,
+    update
+  }
+}
+
+const game = new Phaser.Game(config);
