02ff6902be682cbe3acd828add8d69c6df24eecf
hillarioh <okerioh@gmail.com>
2020-08-30 09:41:34 +0300

    multiple scene

---
+++ src/scene1.js
@@ -0,0 +1,18 @@
+import 'phaser';
+
+class Scene1 extends Phaser.Scene {
+	constructor() {
+		super({ key: 'Scene1' })
+	}
+  
+  create() {
+    this.add.text(110, 150, 'This is the first Scene!', {fill: '#000000', fontSize: '20px'})
+      
+		this.input.on('pointerdown', () => {  
+			this.scene.stop('Scene1')
+			this.scene.start('Scene2')
+		})
+  }
+}
+
+export default Scene1;
