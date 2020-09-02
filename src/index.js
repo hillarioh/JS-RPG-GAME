9d9d318c3be9574b67a6d761fc27ac5ab29e3312
hillarioh <okerioh@gmail.com>
2020-09-02 13:25:08 +0300

    move  config object to index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import 'phaser';
-import config from './config';
 import BootScene from './Scenes/BootScene';
 import WorldScene from './Scenes/WorldScene';
 
+const gameState = {};
+
+const config = {
+  type: Phaser.AUTO,
+  width:800,
+  height:600,
+  backgroundColor: "#ded34a",
+  pixelArt: true,
+  physics: {
+      default: 'arcade',
+      arcade: {
+          gravity: {y: 0},
+          enableBody: true,
+      }
+  },
+  scene: [BootScene,WorldScene]
+};
 
 const game = new Phaser.Game(config);
+
+export {gameState};
+
