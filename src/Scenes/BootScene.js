fcdce8ed5d43d09a5eae947920b5d91fd635bc2d
hillarioh <okerioh@gmail.com>
2020-09-02 13:30:54 +0300

    edit preload in bootscene

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -6,14 +6,7 @@ class BootScene extends Phaser.Scene {
     }
 
     preload(){
-        // map tiles
-        this.load.image('tiles', 'assets/map/spritesheet.png');
-        
-        // map in json format
-        this.load.tilemapTiledJSON('map', 'assets/map/map.json');
-        
-        // our two characters
-        this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
+     
 
     }
 
