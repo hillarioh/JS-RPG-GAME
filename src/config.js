b8b0e4937ad0d92dbff7d22b7e80f8a99645de71
hillarioh <okerioh@gmail.com>
2020-09-02 11:37:23 +0300

    add imports

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
+import BootScene from "./Scenes/BootScene";
+import WorldScene from "./Scenes/WorldScene";
+
 const config = {
     type: Phaser.AUTO,
     width:800,
-    height:600,
+    height:500,
     backgroundColor: "#ded34a",
     physics: {
         default: 'arcade',
@@ -9,7 +12,8 @@ const config = {
             gravity: {y: 800},
             enableBody: true,
         }
-    }
+    },
+    scene: [BootScene,WorldScene]
 };
 
 export default config;
