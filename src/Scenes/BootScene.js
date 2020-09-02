a3adfee990a1c7c83d4285e2bd7547903f34069f
hillarioh <okerioh@gmail.com>
2020-09-02 11:37:39 +0300

    add class methods

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -4,6 +4,22 @@ class BootScene extends Phaser.Scene {
     constructor(){
         super({key: 'BootScene'})
     }
+
+    preload(){
+        // map tiles
+        this.load.image('tiles', 'assets/map/spritesheet.png');
+        
+        // map in json format
+        this.load.tilemapTiledJSON('map', 'assets/map/map.json');
+        
+        // our two characters
+        this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
+
+    }
+
+    create(){
+        this.scene.start('WorldScene');
+    }
 }
 
 export default BootScene;
--- src/Scenes/WorldScene.js
+++ src/Scenes/WorldScene.js
@@ -4,6 +4,14 @@ class WorldScene extends Phaser.Scene {
     constructor(){
         super({key: 'WorldScene'});
     }
+
+    preload(){
+
+    }
+
+    create(){
+        console.log('Here we are');
+    }
 }
 
 export default WorldScene;
