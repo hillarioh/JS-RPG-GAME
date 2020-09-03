328988c95b321887458d9d6f843616ac97fd464f
hillarioh <okerioh@gmail.com>
2020-09-03 19:54:56 +0300

    add menu item and menu class

---
+++ src/menu.js
@@ -0,0 +1,67 @@
+import 'phaser';
+
+class MenuItem extends Phaser.GameObjects.Text {
+    constructor(x,y,text,scene){
+        super(scene,x,y,text,{color: '#ffffff',align: 'left', fontSize:15});
+    }
+
+    select(){
+        this.setColor('#f8ff38');
+    }
+
+    deselect(){
+        this.setColor('#ffffff');
+    }
+
+}
+
+class Menu extends Phaser.GameObjects.Container {
+
+    constructor(x, y, scene, heroes){
+        super( scene, x, y);
+        this.menuItems = [];
+        this.menuItemIndex = 0;
+        this.heroes = heroes;
+        this.x = x;
+        this.y = y;
+    }
+
+    addMenuItem(){
+        let menuItem = new MenuItem(0, this.menuItems.length * 20, unit, this.scene);
+        this.menuItems.push(menuItem);
+        this.add(menuItem); 
+    }
+
+    moveSelectionUp(){
+        this.menuItems[this.menuItemIndex].deselect();
+        this.menuItemIndex--;
+        if(this.menuItemIndex < 0)
+            this.menuItemIndex = this.menuItems.length - 1;
+        this.menuItems[this.menuItemIndex].select();
+    }
+
+    moveSelectionDown() {
+        this.menuItems[this.menuItemIndex].deselect();
+        this.menuItemIndex++;
+        if(this.menuItemIndex >= this.menuItems.length)
+            this.menuItemIndex = 0;
+        this.menuItems[this.menuItemIndex].select();
+    }
+
+    select(index) {
+        if(!index)
+            index = 0;
+        this.menuItems[this.menuItemIndex].deselect();
+        this.menuItemIndex = index;
+        this.menuItems[this.menuItemIndex].select();
+    }
+
+    deselect() {        
+        this.menuItems[this.menuItemIndex].deselect();
+        this.menuItemIndex = 0;
+    }
+
+    confirm() {
+        // wen the player confirms his slection, do the action
+    } 
+}
