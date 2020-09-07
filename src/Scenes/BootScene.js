91aef61de51afa8494cca5f41bd91616be400f9d
hillarioh <okerioh@gmail.com>
2020-09-07 06:47:59 +0300

    load assets

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -8,12 +8,16 @@ class BootScene extends Phaser.Scene {
     preload(){
         this.load.image('dragonblue', 'assets/dragonblue.png');
         this.load.image('dragonorrange', 'assets/orange.png');
-
+        this.load.image('blueButton1', 'assets/ui/blue_button02.png');
+        this.load.image('blueButton2', 'assets/ui/blue_button03.png');
+        this.load.image('box', 'assets/ui/grey_box.png');
+        this.load.image('checkedBox', 'assets/ui/blue_boxCheckmark.png');
+        this.load.audio('bgMusic', ['assets/TownTheme.mp3']);
     }
 
     create(){
         this.scene.stop('BootScene');
-        this.scene.start('WorldScene');
+        this.scene.start('TitleScene');
     }
 }
 
